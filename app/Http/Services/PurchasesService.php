e4a6ab148be792e284bc3e5019b53205aad53fb9
chen po cheng <kkbuger1523@gmail.com>
2021-01-05 19:03:28 +0800

    新增加密函式

--- app/Http/Services/PurchasesService.php
+++ app/Http/Services/PurchasesService.php
@@ -20,7 +20,14 @@ class PurchaseService
     public function getPayload($product,$method)
     {
         $payload = $this ->setPayload($product,$method);
-        return $payload;
+        $encryptedPayload = $this->aesEncrypt($payload->toArray(),$this->hashKey,$this->hashIV);
+        $encodingString = "HashKey=$this->hashKey&$encryptedPayload&HashIV=$this->hashIV";
+        return [
+            'MerchantID'=>$this ->merchantID,
+            'TradeInfo' =>$encryptedPayload,
+            'TradeSha' =>strtoupper(hash("sha256",$encodingString)),
+            'Version'=>1.5
+        ];
     }
 
     public function setPayload($product,$method)
@@ -50,5 +57,29 @@ class PurchaseService
 
         return $payload;
     }
+
+    
+
+    private function aesEncrypt($parameter="",$key="",$iv="")
+    {
+        $returnStr='';
+
+        if(!empty($parameter))
+        {
+            $returnStr=http_build_query($parameter);
+        }
+
+        return trim(bin2hex(openssl_encrypt($this->addpadding($returnStr),'aes-256-cbc',$key,OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING,$iv)));
+    }
+
+    private function addpadding($string, $blocksize = 32)
+    {
+        $len = strlen($string);
+        $pad = $blocksize - ($len % $blocksize);
+        $string .= str_repeat(chr($pad),$pad);
+        return $string;
+    }
+
+    
 }
 
